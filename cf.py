61ebc5f105a250e6778cc2f5f59698d5cf91f679
rouyng <54250428+rouyng@users.noreply.github.com>
2019-08-29 11:45:15 -0700

    callsign input function w/ basic input validation

--- cf.py
+++ cf.py
@@ -11,23 +11,30 @@ from datetime import timedelta
 username = 'KJ7GES'         # should be the user's callsign
 password = ']F_l2;stY![r'   # HamQTH password
 
-def getSession():
-    sessionReq = requests.get('https://www.hamaaaqth.com/xml.php?u={}&p={}'.format(username, password))
-    sessionReq.raise_for_status()               #check whether HTTP request succeeded
+def getsession():
+    sessionReq = requests.get('https://www.hamqth.com/xml.php?u={}&p={}'.format(username, password))
+    sessionReq.raise_for_status()               # check whether HTTP request succeeded
     ## need to handle exception for timeouts/network errors etc here, add retry loop
-    root = ET.fromstring(sessionReq.content)    #get XML tree from HTTPS request results
+    root = ET.fromstring(sessionReq.content)    # get XML tree from HTTPS request results
     sessionID = root[0][0].text
     if sessionID == 'Wrong user name or password':
         print("Wrong user name or password!")
-        sys.exit(1)         # for now, just exit if credentials are wrong. eventually will implement input prompts for new credentials
+        sys.exit(1)         # exit if credentials are wrong. eventually will implement input prompts for new credentials
     else:
         expireTime = datetime.now() + timedelta(hours = 1)
-        print('Session ID: {}\nExpires {}'.format(sessionID, expireTime.strftime('%H:%M:%S'))) # session is valid for one hour, print expiration time when new session is requested
+        print('Connected to HamQTH.com Callsign Database\nSession ID: {}\nExpires {}'.format(sessionID, expireTime.strftime('%H:%M:%S')))  # session is valid for one hour, print expiration time when new session is requested
 
-def getCallsign():
+def inputcallsign():
     callsign = ''
-    input('Enter Callsign to lookup:', callsign)
-    # insert some validation
+    while True:
+        callsign = input('Enter Callsign to Lookup:')
+        if len(callsign) < 3 or len(callsign) > 7:
+            print('{} does not appear to be a valid callsign format, please try again.'.format(callsign))
+            callsign = ''
+            continue
+        else:
+            return callsign
 
-getSession()
-getCallsign()
+
+getsession()
+inputcallsign()
