5a4330f4c1dae2fa6cdc13353ce269fa6d19b05f
rouyng <54250428+rouyng@users.noreply.github.com>
2019-11-17 17:50:05 -0700

    change results view to QTableWidget, add label formatting, add TODOs

--- cf-gui.py
+++ cf-gui.py
@@ -18,34 +18,49 @@ class CfGuiControl:
         self._view = view
         self._configfile = configurationfile
         self._results = {}
+        self._labelfont = QtGui.QFont()
+        self._labelfont.setBold(True)
         self._connectSignals()
+        self._result_data = {'Welcome to CS-Fetch!' : 'Enter a callsign and press "Search" to begin'}
         self._csinput = None
+        self._filltable(self._result_data)
 
     def _connectSignals(self):
-        self._view.searchbutton.clicked.connect(self.getcsinput)
+        self._view.searchbutton.clicked.connect(self._getcsinput)
         self._view.actionAbout.triggered.connect(lambda: QtGui.QDesktopServices.openUrl(QtCore.QUrl('https://github.com/rouyng/callsign-fetch')))
         self._view.actionExit.triggered.connect(lambda: sys.exit())
         self._view.searchinput.returnPressed.connect(self._view.searchbutton.click)
 
-    def getcsinput(self):
-        self._view.listWidget.clear()
+    def _getcsinput(self):
+        self._view.resultsTable.clear()
         self._csinput = self._view.searchinput.text()
+        self._result_data = {}
         if not cf.validatecallsign(self._csinput):
             self._results = cf.fetchcallsigndata(session, self._csinput)
-            if len(self._results) > 0:
-                for key in cf.get_fields_to_print(self._configfile):
-                    if key in self._results.keys() & cf.field_labels.keys():
-                        item = f'{cf.field_labels[key]}: {self._results[key]}'
-                        self._view.listWidget.addItem(item)
-            else:
-                self._view.listWidget.addItem('No result found!')
+            for key in cf.get_fields_to_print(self._configfile):
+                if key in self._results.keys() & cf.field_labels.keys():
+                    self._result_data[cf.field_labels[key]] = self._results[key]
+            self._filltable(self._result_data)
         else:
-            self._view.listWidget.addItem(cf.validatecallsign(self._csinput))
+            self._view.statusbar.showMessage(cf.validatecallsign(self._csinput))
             self._view.searchinput.clear()
+
+    def _filltable(self, data):
+        self._view.resultsTable.setColumnCount(2)
+        self._view.resultsTable.setRowCount(len(data))
+        for i, k in enumerate(data):
+            label_item = QtWidgets.QTableWidgetItem(f'{k}')
+            label_item.setFont(self._labelfont)
+            self._view.resultsTable.setItem(i, 0, label_item)
+            self._view.resultsTable.setItem(i, 1, QtWidgets.QTableWidgetItem(data[k]))
 
     def displaysession(self, sessionid):
         self._view.statusbar.showMessage(f'Session ID: {sessionid}', 0)
 
+# TODO: additional widget for adjusting options within GUI
+# TODO: additional widget for "About" menu item
+# TODO: session timeout/error handling
+# TODO: progress/status info in status bar
 
 if __name__ == '__main__':
     configfile = 'cf.conf'
