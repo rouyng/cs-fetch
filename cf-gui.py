68ecfcb439ccb3da1c5acc75e03aeb301f8f0359
rouyng <54250428+rouyng@users.noreply.github.com>
2019-11-20 17:20:54 -0700

    refactor gui functions, add api/session error handling, add TODOs

--- cf-gui.py
+++ cf-gui.py
@@ -1,6 +1,7 @@
 import sys
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+
 import cf
 from gui.cfguimain import Ui_MainWindow
 
@@ -21,31 +22,50 @@ class CfGuiControl:
         self._labelfont = QtGui.QFont()
         self._labelfont.setBold(True)
         self._connectSignals()
-        self._result_data = {'Welcome to CS-Fetch!' : 'Enter a callsign and press "Search" to begin'}
+        self._result_data = {'Welcome to CS-Fetch!': 'Enter a callsign and press "Search" to begin'}
         self._csinput = None
-        self._filltable(self._result_data)
+        self._session = None
+        self._fillTable(self._result_data)
+        self._InitializeSession()
+
+    def _InitializeSession(self):
+        self._session = cf.initialize(self._configfile)
+        if not self._session:
+            print(f'Could not find configuration file, please check that {self._configfile} exists')
+            sys.exit()
+        else:
+            self._sessionStatus(self._session)
+
 
     def _connectSignals(self):
-        self._view.searchbutton.clicked.connect(self._getcsinput)
+        self._view.searchbutton.clicked.connect(self._getCallsignInput)
         self._view.actionAbout.triggered.connect(lambda: QtGui.QDesktopServices.openUrl(QtCore.QUrl('https://github.com/rouyng/callsign-fetch')))
         self._view.actionExit.triggered.connect(lambda: sys.exit())
         self._view.searchinput.returnPressed.connect(self._view.searchbutton.click)
 
-    def _getcsinput(self):
+    def _getCallsignInput(self):
         self._view.resultsTable.clear()
         self._csinput = self._view.searchinput.text()
         self._result_data = {}
         if not cf.validatecallsign(self._csinput):
-            self._results = cf.fetchcallsigndata(session, self._csinput)
-            for key in cf.get_fields_to_print(self._configfile):
-                if key in self._results.keys() & cf.field_labels.keys():
-                    self._result_data[cf.field_labels[key]] = self._results[key]
-            self._filltable(self._result_data)
+            self._results = cf.fetchcallsigndata(self._session, self._csinput)
+            if self._results.__class__ == str:
+                # if the results of cf.fetchcallsigndata() are a string, the API has produced an error
+                self._view.resultsTable.setColumnCount(0)
+                self._view.resultsTable.setRowCount(0)
+                self._errorStatus(self._results)
+                if 'Session does not exist or expired' in self._results:
+                    cf.initialize(self._configfile)
+            else:
+                for key in cf.get_fields_to_print(self._configfile):
+                    if key in self._results.keys() & cf.field_labels.keys():
+                        self._result_data[cf.field_labels[key]] = self._results[key]
+                self._fillTable(self._result_data)
         else:
             self._view.statusbar.showMessage(cf.validatecallsign(self._csinput))
             self._view.searchinput.clear()
 
-    def _filltable(self, data):
+    def _fillTable(self, data):
         self._view.resultsTable.setColumnCount(2)
         self._view.resultsTable.setRowCount(len(data))
         for i, k in enumerate(data):
@@ -57,13 +77,19 @@ class CfGuiControl:
             else:
                 self._view.resultsTable.setItem(i, 1, QtWidgets.QTableWidgetItem(data[k]))
 
-    def displaysession(self, sessionid):
+    def _sessionStatus(self, sessionid):
         self._view.statusbar.showMessage(f'Session ID: {sessionid}', 0)
 
+    def _errorStatus(self, error_text):
+        self._view.statusbar.showMessage(f'Error: {error_text}', 5000) # show error in status bar for 5 sec
+
+# TODO: Request timer ('results returned in x sec' in status bar)
+# TODO: make results table read-only
 # TODO: additional widget for adjusting options within GUI
 # TODO: additional widget for "About" menu item
 # TODO: session timeout/error handling
 # TODO: progress/status info in status bar
+
 
 if __name__ == '__main__':
     configfile = 'cf.conf'
@@ -71,12 +97,6 @@ if __name__ == '__main__':
     application = cfMainWindow()
     application.show()
     controller = CfGuiControl(view=application.ui, configurationfile=configfile)
-    session = cf.initialize(configfile)
-    if not session:
-        print(f'Could not find configuration file, please check that {configfile} exists')
-        sys.exit()
-    else:
-        controller.displaysession(session)
     sys.exit(app.exec_())
 
 
