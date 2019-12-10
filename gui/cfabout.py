accabe6c5ab6051aed28c4975a2bd2bb04ef9c0c
rouyng <54250428+rouyng@users.noreply.github.com>
2019-12-09 17:37:51 -0700

    tweak gui appearance, rewrite about menu text, set all fields to be shown in default cf.conf file

--- gui/cfabout.py
+++ gui/cfabout.py
@@ -13,7 +13,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_aboutDialog(object):
     def setupUi(self, aboutDialog):
         aboutDialog.setObjectName("aboutDialog")
-        aboutDialog.resize(400, 169)
+        aboutDialog.resize(411, 240)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -21,25 +21,19 @@ class Ui_aboutDialog(object):
         aboutDialog.setSizePolicy(sizePolicy)
         aboutDialog.setWhatsThis("")
         self.okButton = QtWidgets.QPushButton(aboutDialog)
-        self.okButton.setGeometry(QtCore.QRect(160, 130, 75, 23))
+        self.okButton.setGeometry(QtCore.QRect(170, 200, 75, 23))
         self.okButton.setObjectName("okButton")
         self.titleLabel = QtWidgets.QLabel(aboutDialog)
-        self.titleLabel.setGeometry(QtCore.QRect(160, 10, 61, 21))
+        self.titleLabel.setGeometry(QtCore.QRect(170, 10, 61, 21))
         font = QtGui.QFont()
         font.setPointSize(12)
         self.titleLabel.setFont(font)
         self.titleLabel.setObjectName("titleLabel")
-        self.authorLabel = QtWidgets.QLabel(aboutDialog)
-        self.authorLabel.setGeometry(QtCore.QRect(130, 30, 121, 21))
-        self.authorLabel.setObjectName("authorLabel")
-        self.githubLabel = QtWidgets.QLabel(aboutDialog)
-        self.githubLabel.setGeometry(QtCore.QRect(20, 70, 131, 21))
-        self.githubLabel.setOpenExternalLinks(True)
-        self.githubLabel.setObjectName("githubLabel")
-        self.apiLabel = QtWidgets.QLabel(aboutDialog)
-        self.apiLabel.setGeometry(QtCore.QRect(180, 70, 201, 21))
-        self.apiLabel.setOpenExternalLinks(True)
-        self.apiLabel.setObjectName("apiLabel")
+        self.label = QtWidgets.QLabel(aboutDialog)
+        self.label.setGeometry(QtCore.QRect(10, 40, 391, 161))
+        self.label.setWordWrap(True)
+        self.label.setOpenExternalLinks(True)
+        self.label.setObjectName("label")
 
         self.retranslateUi(aboutDialog)
         QtCore.QMetaObject.connectSlotsByName(aboutDialog)
@@ -49,9 +43,11 @@ class Ui_aboutDialog(object):
         aboutDialog.setWindowTitle(_translate("aboutDialog", "About Cs-Fetch"))
         self.okButton.setText(_translate("aboutDialog", "Ok"))
         self.titleLabel.setText(_translate("aboutDialog", "Cs-Fetch"))
-        self.authorLabel.setText(_translate("aboutDialog", "by Ross Young, KJ7GES"))
-        self.githubLabel.setText(_translate("aboutDialog", "<a href=\"https://github.com/rouyng/callsign-fetch\">View this project on GitHub</a>"))
-        self.apiLabel.setText(_translate("aboutDialog", "<html><head/><body><p>This software uses the <a href=\"https://www.hamqth.com/developers.php\"><span style=\" text-decoration: underline; color:#0000ff;\">HamQTH.com API</span></a></p></body></html>"))
+        self.label.setText(_translate("aboutDialog", "<p>Cs-Fetch  is a simple amateur radio callsign lookup tool, created by Ross Young, KJ7GES. For more information, please see README.md, or <a href=\"https://github.com/rouyng/callsign-fetch\">visit Cs-Fetch on GitHub</a>. This is free, open source software built with Python. Bug reports and code contributions are welcomed through the above mentioned GitHub repository.</p>  \n"
+"<p>Cs-Fetch uses the <a href=\"https://www.hamqth.com/developers.php\">HamQTH.com API</a> to look up details on amateur radio callsigns. Cs-Fetch is an independent project and is not otherwise associated with HamQTH.com or its staff.\n"
+"<p> This software is licensed under the conditions of <a href=\"https://www.gnu.org/licenses/gpl-3.0.en.html\">the GNU GPL v3</a>. See LICENSE.md for details.</p>\n"
+"\n"
+""))
 
 
 if __name__ == "__main__":
