f213fe0341722c7a1eb6d45d0454a9efc6ee0425
Ross <54250428+rouyng@users.noreply.github.com>
2020-01-02 15:24:25 -0700

    add docstrings

--- cfgui.py
+++ cfgui.py
@@ -61,6 +61,7 @@ class CfGuiControl:
         self._connect_signals()
 
     def _connect_signals(self):
+        """Connect GUI button/menu signals"""
         # signals related to main widget
         self._main_view.searchbutton.clicked.connect(self._process_callsign_input)
         self._main_view.actionExit.triggered.connect(lambda: sys.exit())
@@ -74,12 +75,14 @@ class CfGuiControl:
         self._options_view.cancelButton.clicked.connect(lambda: self._options_cancel())
 
     def _check_session_status(self):
+        """Checks if the current session has an error message and displays the appropriate status bar message"""
         if self._session.session_error:
             self._show_error_status(f'{self._session.session_error.__str__()[:78]}')
         else:
             self._show_session_status(self._session.session_id)
 
     def _process_callsign_input(self):
+        """Read user's input for callsign search, run validation, fetch results and display results or errors"""
         self._main_view.resultsTable.clear()
         self._main_view.resultsTable.setColumnCount(0)
         self._main_view.resultsTable.setRowCount(0)
@@ -111,7 +114,8 @@ class CfGuiControl:
             self._show_error_status(validation_failed)
             self._main_view.searchinput.clear()
 
-    def _fill_results_table(self, data):
+    def _fill_results_table(self, data: dict):
+        """Populate results table with callsign information"""
         self._main_view.resultsTable.setColumnCount(2)
         self._main_view.resultsTable.setRowCount(len(data))
         for i, k in enumerate(data):
@@ -124,27 +128,31 @@ class CfGuiControl:
                 self._main_view.resultsTable.setItem(i, 1, QtWidgets.QTableWidgetItem(data[k]))
 
     def _show_session_status(self, sessionid):
+        """Clear current status bar widget and display session status message"""
         self._main_view.statusbar.removeWidget(self._status_widget)
         self._status_widget = QtWidgets.QLabel(f'Session ID: {sessionid}')
         self._main_view.statusbar.addWidget(self._status_widget)
 
     def _show_error_status(self, error_text):
+        """Clear current status bar widget and display error message"""
         self._main_view.statusbar.removeWidget(self._status_widget)
         self._status_widget = QtWidgets.QLabel(f'Error: {error_text}')
         self._main_view.statusbar.addWidget(self._status_widget)
 
     def _show_timer_status(self, time):
+        """Clear current status bar widget and display message with time to retrieve callsign results"""
         self._main_view.statusbar.removeWidget(self._status_widget)
         self._status_widget = QtWidgets.QLabel(f'Retrieved callsign info in {time:.2f} seconds')
         self._main_view.statusbar.addWidget(self._status_widget)
 
     def _show_progress_status(self, entered_callsign):
+        """Clear current status bar widget and display message showing callsign info retrieval is in progress"""
         self._main_view.statusbar.removeWidget(self._status_widget)
         self._status_widget = QtWidgets.QLabel(f'Retrieving information about {entered_callsign}')
         self._main_view.statusbar.addWidget(self._status_widget)
 
     def _load_config(self):
-        # load options from active session and display them in appropriate widgets
+        """Load options from active session and display them in appropriate widgets within the Options window"""
         self._fields = [self._session.field_labels[f] for f in self._session.field_list]
         for b in self._options_view.scrollAreaWidgetContents.children():
             if isinstance(b, QtWidgets.QCheckBox):
@@ -164,7 +172,7 @@ class CfGuiControl:
             self._options_view.lightButton.setChecked(True)
 
     def _save_config(self):
-        # save options from GUI to cf.conf, hide view and reinitialize
+        """Save options from Options Window to cf.conf, then hide window and reinitialize session"""
         new_fields = []
         source = 'hamqth'
         if self._options_view.qthButton.isChecked():
@@ -191,7 +199,7 @@ class CfGuiControl:
         self._check_session_status()
 
     def _options_cancel(self):
-        # hide view and reload config from file when cancel button is pressed
+        """Hide view and reload config from file when cancel button is pressed"""
         self._options_widget.close()
         self._load_config()
 
